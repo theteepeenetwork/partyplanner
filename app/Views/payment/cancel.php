31128bff185a2b4b4d94ad4be63943fc49d32130
theteepeenetwork <markjpearson@me.com>
2024-09-08 09:40:37 +0100

    Stripe is working. $15 deposit confirms.

--- app/Views/payment/cancel.php
+++
@@ -1,2 +0,0 @@
-<h1>Payment Cancelled</h1>
-<p>Your payment was cancelled. Please try again.</p>
--- app/Views/profile/vendor_calendar.php
+++
@@ -1,4 +0,0 @@
-<div class="tab-pane fade" id="calendar" role="tabpanel" aria-labelledby="calendar-tab">
-    <h2>Booking Calendar</h2>
-    <?= $this->include('vendor_calendar') ?>
-</div>
