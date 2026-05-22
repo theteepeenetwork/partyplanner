681d92c1e1f49ae17f01d9098bb40593e20d73cb
theteepeenetwork <76913304+theteepeenetwork@users.noreply.github.com>
2026-05-22 16:11:07 +0100

    database

--- createvent/app/Config/Database.php
+++ createvent/app/Config/Database.php
@@ -26,7 +26,7 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => '127.0.0.1',
         'username'     => 'root',
         'password'     => 'root',
         'database'     => 'event_marketplace',
